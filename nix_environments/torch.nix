06e13d49c69710cf4cac866b69bda0fb1d1ede50
arielherself <arielherself@duck.com>
2025-03-15 18:55:59 +0800

    regular backup

--- nix_environments/torch.nix
+++ nix_environments/torch.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 (pkgs.buildFHSEnv {
 	name = "torch_env";
 	targetPkgs = pkgs: with pkgs; [
-		cudatoolkit
+		# cudatoolkit
 		libGLU
 		libGL
 		stdenv.cc
@@ -20,7 +20,7 @@ with import <nixpkgs> {};
 	profile = ''
 		alias python="nixGL /usr/bin/python"  # You should install nixGL first
 		export PATH=/usr/bin:$PATH
-		export CUDA_PATH=${pkgs.cudatoolkit}
+		# export CUDA_PATH=${pkgs.cudatoolkit}
 		export EXTRA_CCFLAGS="-I/usr/include"
 
 		source $HOME/.bashrc
